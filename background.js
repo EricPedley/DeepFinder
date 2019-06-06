8e152e6528e9cfc67a351656f96bad0ae7313cbb
ericpedley@gmail.com <ericpedley@gmail.com>
2019-06-05 21:35:32 -0700

    works on all sites now

--- background.js
+++ background.js
@@ -1,21 +1,24 @@
 
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-      "id": "sampleContextMenu",
-      "title": "Sample Context Menu",
-      "contexts": ["selection"]
-    });
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    "id": "sampleContextMenu",
+    "title": "Sample Context Menu",
+    "contexts": ["selection"]
+  });
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ color: '#3aa757' }, function () {
+    console.log("The color is green.");
   });
 
-  chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  });
-
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
-          actions: [new chrome.declarativeContent.ShowPageAction()]
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+      })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
+});
