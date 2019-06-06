c5b7b7fbf1d73756042651f29ff33b3bd9d7fb54
ericpedley@gmail.com <ericpedley@gmail.com>
2019-06-05 17:49:44 -0700

    changed declarative content part again

--- background.js
+++ background.js
@@ -14,12 +14,8 @@ chrome.runtime.onInstalled.addListener(function() {
     });
   });
 
-  // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-  //   chrome.declarativeContent.onPageChanged.addRules([{
-  //     conditions: [new chrome.declarativeContent.PageStateMatcher({
-  //       pageUrl: {hostEquals: 'developer.chrome.com'},
-  //     })
-  //     ],
-  //         actions: [new chrome.declarativeContent.ShowPageAction()]
-  //   }]);
-  // });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
