e46edde0ea7f4e3b554a0ee084c6e27f1d35303c
ericpedley@gmail.com <ericpedley@gmail.com>
2019-06-16 13:59:30 -0700

    idk what these changes are honestly

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
 		alert("message code started");
 		let replacement = "<span style='background-color:yellow'>" + request + "</span>";
-		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)'+request,"g"), replacement);
+		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)'+request,["g","i"]), replacement);
 		document.documentElement.innerHTML = replaced;
 		//links = document.getElementsByTagName('a');
 	}
