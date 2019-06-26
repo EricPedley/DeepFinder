caed3bda921b8f2b29c91da01f044ce746eadec7
Eric Pedley <ericpedley@Mays-MacBook-Air.local>
2019-06-26 13:54:19 -0700

    removed the debugging alerts

--- popup.js
+++ popup.js
@@ -1,34 +1,21 @@
+var openedIDs = [];
 
-var results;
-chrome.tabs.getSelected(null,function(tab){
+function searchOnTab(searchTerm, tab) {
     chrome.tabs.executeScript(
         tab.id,
-        { file: 'content.js' }
+        {file:'content.js'},
+        function(results) {
+            chrome.tabs.sendMessage(tab.id,searchTerm);
+        }
     );
-});
+};
+
 $(document).ready(function () {
     $("#textbox").keypress(function (event) {
         if (event.keyCode === 13) {
-            alert("event seen")
             chrome.tabs.getSelected(null, function (tab) {
-                
                 let search = $("#textbox").val();
-                chrome.tabs.sendMessage(tab.id, search);
-                // chrome.tabs.executeScript(
-                //     tab.id,
-                //     {code: 'var x=document.getElementsByTagName("html")[0].innerText; x',},
-                //     function(r) {
-                //         results=r;
-                //         let search = $("#textbox").val();
-                //         let replacement = "<span style='background-color:yellow'>"+search+"</span>";
-                //         let replaced = (""+results).replace(new RegExp(search,"g"),replacement);
-                //         chrome.tabs.executeScript(
-                //             tab.id,
-                //             {file:'content.js'}
-                //         );
-                //         chrome.tabs.sendMessage(tab.id,replaced);
-                //     }
-                // );
+                searchOnTab(search,tab);
             });
         }
     });
