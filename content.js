d3fc51d05cc428b17bd884664bdd73acd7e306fb
Eric Pedley <ericpedley@Mays-MacBook-Air.local>
2019-06-26 13:54:35 -0700

    added code to get all external links

--- content.js
+++ content.js
@@ -1,10 +1,14 @@
-
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
-		alert("message code started");
 		let replacement = "<span style='background-color:yellow'>" + request + "</span>";
 		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)'+request,"g"), replacement);
 		document.documentElement.innerHTML = replaced;
 		links = document.getElementsByTagName('a');
+		for(var i=0;i<links.length;i++){
+			if(!links[i].href.contains("#")){//if the link is extenal
+				window.open(links[i].href);
+				break;
+			}
+		}
 	}
 );
