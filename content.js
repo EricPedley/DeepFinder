f2c7d7ef9c9ce99df783d926636a9eb389897b41
ericpedley@gmail.com <ericpedley@gmail.com>
2019-06-30 11:53:50 -0700

    idk what i did here

--- content.js
+++ content.js
@@ -3,8 +3,9 @@ chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
 		alert("message code started");
 		let replacement = "<span style='background-color:yellow'>" + request + "</span>";
-		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)'+request,["g","i"]), replacement);
+		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)'+request,"g"), replacement);
 		document.documentElement.innerHTML = replaced;
-		//links = document.getElementsByTagName('a');
+		links = document.getElementsByTagName('a');
+		alert($(links[0]).href());
 	}
 );
