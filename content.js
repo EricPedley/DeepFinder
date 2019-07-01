297c369ab6265c25843951302a3e9200fe80bf7f
ericpedley@gmail.com <ericpedley@gmail.com>
2019-06-30 19:44:26 -0700

    made it open external links

--- content.js
+++ content.js
@@ -4,11 +4,20 @@ chrome.runtime.onMessage.addListener(
 		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)'+request,"g"), replacement);
 		document.documentElement.innerHTML = replaced;
 		links = document.getElementsByTagName('a');
+		//window.open(links[1].href);
+		console.log((""+links[1].href).includes("#"));
+		console.log("hello,world!");
 		for(var i=0;i<links.length;i++){
-			if(!links[i].href.contains("#")){//if the link is extenal
-				window.open(links[i].href);
-				break;
+			if(!(""+links[i].href).includes("#")){//if the link is extenal
+				let win = window.open(links[i].href);
+				if(win.document.documentElement.innerHTML.includes(request)){
+					alert("eyyyyyy");
+					win.close();
+					break;
+				}
+				win.close();
 			}
 		}
+		sendResponse(true);
 	}
 );
--- popup.js
+++ popup.js
@@ -1,21 +1,32 @@
 var openedIDs = [];
 
-function searchOnTab(searchTerm, tab) {
-    chrome.tabs.executeScript(
-        tab.id,
-        {file:'content.js'},
-        function(results) {
-            chrome.tabs.sendMessage(tab.id,searchTerm);
-        }
-    );
+function searchOnTab(searchTerm, tab) {//sends messasge to tab and if the listener isn't there it adds the listener and sends the message again 
+    // chrome.tabs.sendMessage(tab.id,searchTerm,function(response){
+    //     if(!response){
+    //         chrome.tabs.executeScript(
+    //             tab.id,
+    //             {file:'content.js'},
+    //             function(){
+    //                 chrome.tabs.sendMessage(tab.id,searchTerm);
+    //             }
+    //         );
+    //     }
+    // });
+    
 };
 
 $(document).ready(function () {
+    chrome.tabs.getSelected(null, function(tab){
+        chrome.tabs.executeScript(
+            tab.id,
+            {file:'content.js'}
+        );
+    });
     $("#textbox").keypress(function (event) {
         if (event.keyCode === 13) {
             chrome.tabs.getSelected(null, function (tab) {
                 let search = $("#textbox").val();
-                searchOnTab(search,tab);
+                chrome.tabs.sendMessage(tab.id,search);
             });
         }
     });
