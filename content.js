1aa7a2e967a703c3f7e99049ff3ae84140db20ab
ericpedley@gmail.com <ericpedley@gmail.com>
2019-07-01 23:46:59 -0700

    continued trying to make the external links feature work

--- content.js
+++ content.js
@@ -7,17 +7,25 @@ chrome.runtime.onMessage.addListener(
 		//window.open(links[1].href);
 		console.log((""+links[1].href).includes("#"));
 		console.log("hello,world!");
-		for(var i=0;i<links.length;i++){
-			if(!(""+links[i].href).includes("#")){//if the link is extenal
-				let win = window.open(links[i].href);
+		let win = window.open(links[1].href);
+		console.log(win.document.documentElement.innerHTML+"");
 				if(win.document.documentElement.innerHTML.includes(request)){
 					alert("eyyyyyy");
 					win.close();
-					break;
+				} else {
+					win.close();
 				}
-				win.close();
-			}
-		}
+		// for(var i=0;i<links.length;i++){
+		// 	if(!(""+links[i].href).includes("#")){//if the link is extenal
+		// 		let win = window.open(links[i].href);
+		// 		if(win.document.documentElement.innerHTML.includes(request)){
+		// 			alert("eyyyyyy");
+		// 			win.close();
+		// 			break;
+		// 		}
+		// 		win.close();
+		// 	}
+		// }
 		sendResponse(true);
 	}
 );
