12a837256dd062fa0ede71c130309c903bcd8637
ericpedley@gmail.com <ericpedley@gmail.com>
2019-07-06 12:25:15 -0700

    EYY EZ PZ IT PASSES BACK THE INNERHTML

--- content.js
+++ content.js
@@ -1,17 +1,19 @@
+console.log("script ready");
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
-
-		let replacement = "<span style='background-color:yellow'>" + request + "</span>";
-		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)' + request, "g"), replacement);
+		let replacement = "<span style='background-color:yellow'>" + request.greeting + "</span>";
+		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)' + request.greeting, "g"), replacement);
 		document.documentElement.innerHTML = replaced;
 		links = document.getElementsByTagName('a');
 		let win = window.open(links[1].href, 'targetWindow', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=400,height=600');
-		var h  = ""+win.document.documentElement.innerHTML;
-		win.close();
-		alert(h);
-
-		sendResponse({ farewell: "goodbye" });
+		var h;
+		h = "not loaded yet";
+		win.onload = function () {
+			h = "" + win.document.getElementsByTagName("html")[0].innerHTML;
+			
+			sendResponse({ ey: h });
+			console.log("onload ran");
+			win.close();
+		}
 		return true;
-	}
-);
-
+	});
--- popup.js
+++ popup.js
@@ -1,23 +1,38 @@
 
 $(document).ready(function () {
 
-    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        if (changeInfo.status == 'complete') {
-            chrome.tabs.executeScript(
-                tabId,
-                { file: 'content.js' }
-            );
+    // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    //     if (changeInfo.status == 'complete') {
+    //         chrome.tabs.executeScript(
+    //             tabId,
+    //             { file: 'content.js' }
+    //         );
 
-        }
-    });
+    //     }
+    // });
 
     $("#textbox").keypress(function (event) {
         if (event.keyCode === 13) {
             let search = $("#textbox").val();
-            chrome.tabs.getSelected(null, function (tab) {
-                chrome.tabs.sendMessage(tab.id, search);
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { greeting: search }, function (response) {
+                    if ("response" in window)
+                        console.log(response.ey);
+                    else {
+                        chrome.tabs.executeScript(
+                            tabs[0].id,
+                            { file: 'content.js' },
+                            function () {
+                                chrome.tabs.sendMessage(tabs[0].id, { greeting: search }, function (response) {
+                                    console.log(response.ey);
+                                });
+                            }
+                        );
+                    }
+                });
             });
         }
+
     });
 
 
