d7d10a7213b5f09de58e29a587ca3f4dd4348e2c
ericpedley@gmail.com <ericpedley@gmail.com>
2019-07-07 19:10:21 -0700

    used jquery and it fixes everything

--- popup.js
+++ popup.js
@@ -1,39 +1,27 @@
 
 $(document).ready(function () {
-
-    // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    //     if (changeInfo.status == 'complete') {
-    //         chrome.tabs.executeScript(
-    //             tabId,
-    //             { file: 'content.js' }
-    //         );
-
-    //     }
-    // });
-
     $("#textbox").keypress(function (event) {
         if (event.keyCode === 13) {
             let search = $("#textbox").val();
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, { greeting: search }, function (response) {
                     if ("response" in window)
-                        console.log(response.ey);
+                        console.log(response);
                     else {
-                        chrome.tabs.executeScript(
-                            tabs[0].id,
-                            { file: 'content.js' },
-                            function () {
-                                chrome.tabs.sendMessage(tabs[0].id, { greeting: search }, function (response) {
-                                    console.log(response.ey);
-                                });
-                            }
-                        );
+                        console.log("no response");
+                        // chrome.tabs.executeScript(
+                        //     tabs[0].id,
+                        //     { file: 'content.js' },
+                        //     function () {
+                        //         chrome.tabs.sendMessage(tabs[0].id, { greeting: search }, async function (response) {
+                        //             console.log(response);
+                        //         });
+                        //     }
+                        // );
                     }
                 });
             });
         }
 
     });
-
-
 });
