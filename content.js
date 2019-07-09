ba8657d9b7bc5937f5a64269b966f8a3635e1e87
ericpedley@gmail.com <ericpedley@gmail.com>
2019-07-09 14:56:49 -0700

    Now links show up on popup, not just in console

--- content.js
+++ content.js
@@ -1,54 +1,30 @@
-console.log("script ready!!!!!");
+
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
-
 		let replacement = "<span style='background-color:yellow'>" + request.greeting + "</span>";
 		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)' + request.greeting, "g"), replacement);
 		document.documentElement.innerHTML = replaced;
-		
+
 		var linkList = document.getElementsByTagName('a');
-		var externalPageHTMLs = { "key1": "value1" };
-		var linksProcessed = 0;
-		var linksToProcess = 0;
+		var externalPageHTMLs = [];//{ "key1": "value1" };
+		var itemsStarted = 0;
+		var itemsCompleted = 0;
 		Array.from(linkList).forEach(async function (link) {
-			//console.log("loop iteration ran");
 			if (!link.href.includes("#")) {
-				$.get(link.href,null, function(text) {
-					if(null!==text.match(new RegExp('(?<!<[^>]*)' + request.greeting, "g"))){
+				itemsStarted++;
+				$.get(link.href, null, function (text) {
+					itemsCompleted++;
+					if (null !== text.match(new RegExp('(?<!<[^>]*)' + request.greeting, "g"))) {
 						console.log(link.href);
+						externalPageHTMLs.push(link.href);
 					}
-					externalPageHTMLs[link.href] = text;
-					//11sendResponse({farewell:"goodbye"});
+					if (itemsStarted == itemsCompleted)
+						sendResponse(externalPageHTMLs);
 				})
-				//window.open(link.href, 'targetWindow', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=100,height=100');
-				//window.focus();
-				// let win = window.open(link.href, 'targetWindow', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=100,height=100');
-				// window.focus();
-				// linksToProcess++;
-				// win.onload = function () {
-				// 	console.log("onload called");
-				// 	externalPageHTMLs[link.href] = win.document.getElementsByTagName("html")[0].innerHTML;
-				// 	win.close();
-				// 	linksProcessed++;
-				// 	console.log(linksToProcess + "/" + linksProcessed);
-				// 	if (linksToProcess == linksProcessed) {
-				// 		sendResponse(externalPageHTMLs);
-				// 		console.log("response sent");
-				// 	}
-				// };
+
 			}
+
 		});
-		
-		//sendResponse(externalPageHTMLs);
 		return true;
 
-		// var links = document.getElementsByTagName('a');
-		// let win = window.open(links[1].href, 'targetWindow', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=400,height=600');
-		// var h = "not loaded yet";
-		// win.onload = function () {
-		// 	h = "" + win.document.getElementsByTagName("html")[0].innerHTML;
-		// 	sendResponse({ ey: h });
-		// 	console.log("onload ran");
-		// 	win.close();
-		// }
 	});
--- popup.js
+++ popup.js
@@ -1,24 +1,17 @@
-
 $(document).ready(function () {
     $("#textbox").keypress(function (event) {
         if (event.keyCode === 13) {
             let search = $("#textbox").val();
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, { greeting: search }, function (response) {
-                    if ("response" in window)
-                        console.log(response);
-                    else {
-                        console.log("no response");
-                        // chrome.tabs.executeScript(
-                        //     tabs[0].id,
-                        //     { file: 'content.js' },
-                        //     function () {
-                        //         chrome.tabs.sendMessage(tabs[0].id, { greeting: search }, async function (response) {
-                        //             console.log(response);
-                        //         });
-                        //     }
-                        // );
-                    }
+                    console.log(response);
+                    response.forEach(function (linkItem,index) {
+                        document.getElementById("putLinksHere").innerHTML += "<a id = 'link"+index+"' href = '"+linkItem+"'>"+linkItem+"</a><br>";
+                        $("#link"+index).on('click',function() {
+                            window.open(linkItem);
+                            console.log("onclick event fired for "+index);
+                        });
+                    });
                 });
             });
         }
