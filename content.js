927a871cda96e3532cf94b286ff5f7267fdfce1e
ericpedley@gmail.com <ericpedley@gmail.com>
2019-07-27 13:16:48 -0700

    Moved http requests to popup script from content script

--- content.js
+++ content.js
@@ -1,37 +1,17 @@
 
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
-		let replacement = "<span style='background-color:yellow'>" + request.greeting + "</span>";
-		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)' + request.greeting, "g"), replacement);
+		let replacement = "<span style='background-color:"+request.color+"'>" + request.greeting + "</span>";
+		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)' + request.greeting,(request.checkboxStatus? "g":"gi")), replacement);
 		document.documentElement.innerHTML = replaced;
 
-		var linkList = document.getElementsByTagName('a');
-		var externalPageHTMLs = [];//{ "key1": "value1" };
-		var itemsStarted = 0;
-		var itemsCompleted = 0;
-		Array.from(linkList).forEach(async function (link) {
-			if (!link.href.includes("#")&&link.href.includes("https")) {
-				itemsStarted++;
-				$.get(link.href, null, function (text) {
-					itemsCompleted++;
-					let regex;
-					if(request.checkboxStatus)
-						regex = new RegExp('(?<!<[^>]*)' + request.greeting, "g");
-					else
-						regex = new RegExp('(?<!<[^>]*)' + request.greeting, "gi")
-					if (null !== text.match(regex)) {
-						console.log(link.href);
-						externalPageHTMLs.push(link.href);
-					}
-					if (itemsStarted == itemsCompleted) {
-						sendResponse(externalPageHTMLs);
-						break;
-					}
-				})
-
-			}
-
+		var linkList=[];
+		Array.from(document.getElementsByTagName('a')).forEach(function(link) {
+			linkList.push(link.href);
 		});
+		console.log(linkList);
+		linkList.push(window.location.href);
+		sendResponse(linkList);
 		return true;
 
 	});
--- popup.js
+++ popup.js
@@ -2,25 +2,37 @@ $(document).ready(function () {
     $("#textbox").keypress(function (event) {
         if (event.keyCode === 13) {
             let search = $("#textbox").val();
+            var checked = document.getElementById("case").checked;
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, 
-                    { greeting: search,
-                      checkboxStatus: document.getElementById("case").checked 
-                      color: document.getElementById("color").value}, function (response) {
-                    console.log(response);
-                    document.getElementById("putLinksHere").innerHTML = "<center>Links Containing Keyword:</center>";
-                    let forEachRan = false;
-                    response.forEach(function (linkItem,index) {
-                        forEachRan=true;
-                        document.getElementById("putLinksHere").innerHTML += "<a id = 'link"+index+"' href = '"+linkItem+"'>"+linkItem+"</a><br>";
-                        $("#link"+index).on('click',function() {
-                            window.open(linkItem);
-                            console.log("onclick event fired for "+index);
+
+                chrome.tabs.sendMessage(
+                    tabs[0].id,
+                    {
+                        greeting: search,
+                        checkboxStatus: checked,
+                        color: document.getElementById("color").style.backgroundColor
+                    },
+                    function (response) {
+                        console.log(response);
+                        document.getElementById("putLinksHere").innerHTML = "<center>Links Containing Keyword:</center>";
+                        let forEachRan = false;
+                        var winhref = response.pop();
+                        response.forEach(async function (link, index) {
+                            forEachRan=true;
+                            if (!link.includes("#")) {
+
+                                console.log(link);
+                                var regex = new RegExp('(?<!<[^>]*)' + search, (checked ? "g" : "gi"))
+                                $.get(link, null, function (text) {
+                                    if (null !== text.match(regex)) {
+                                        document.getElementById("putLinksHere").innerHTML += "<br> <a id = 'link" + index + "' href = '" + link + "'>" + link + "</a> <br>";
+                                    }
+                                });
+                            }
                         });
+                        if (!forEachRan)
+                            document.getElementById("putLinksHere").innerHTML = "<center>Keyword not found in links</center>";
                     });
-                    if(!forEachRan)
-                        document.getElementById("putLinksHere").innerHTML = "<center>Keyword not found in links</center>";
-                });
             });
         }
 
