559053e0b87385505af61f7e4b48e164a46667b6
Eric Pedley <ericpedley@Mays-Air.attlocal.net>
2019-09-05 20:35:15 -0700

    Fixed links showing up as undefined

--- content.js
+++ content.js
@@ -2,49 +2,59 @@ console.log("this better be happening");
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
 		console.log("message received, stupid");
-		let replacement = "<span style='background-color:"+request.color+"'>" + request.searchTerm + "</span>";//highlights keyword on page
-		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)' + (request.checkboxStatus[1]? "\b"+request.searchTerm+"\b": request.searchTerm),(request.checkboxStatus[0]? "g":"gi")), replacement);
+		let replacement = "<span style='background-color:" + request.color + "'>" + request.searchTerm + "</span>";//highlights keyword on page
+		let replaced = ("" + document.documentElement.innerHTML).replace(new RegExp('(?<!<[^>]*)' + (request.checkboxStatus[1] ? "\b" + request.searchTerm + "\b" : request.searchTerm), (request.checkboxStatus[0] ? "g" : "gi")), replacement);
 		document.documentElement.innerHTML = replaced;
-		
-		var linkList=[];
+
+		var aTags = document.getElementsByTagName('a');
+		var linkList = [];
 		var frames = Array.from(document.getElementsByTagName("frame"));
 		frames = frames.concat(Array.from(document.getElementsByTagName("iframe")));
-		let c1=0;
-		frames.forEach(async function(frame) {
+		let c1 = 0;
+		frames.forEach(async function (frame) {
 			c1++;
 			frame.onload = () => {
-				if(frame.contentDocument) {
-					frame.document=frame.contentDocument;
+				if (frame.contentDocument) {
+					frame.document = frame.contentDocument;
 				}
 				let frameBody = frame.document.body;
 				let frameLinks = Array.from(frameBody.getElementsByTagName("a"));//gets all links from frames on page
-				let c2=0;
+				let c2 = 0;
 				frameLinks.forEach((link) => {
-					linkList.push({href: link.href, innerHTML: link.innerHTML});
+					linkList.push({ href: link.href, innerHTML: link.innerHTML });
 					c2++;
-					if(c1==frames.length&&c2==frameLinks.length) {
+					if (c1 == frames.length && c2 == frameLinks.length) {
 						linkList.push(window.location.href);
-						sendResponse(linkList);
+						if (aTags !== null) {
+							Array.from(aTags).forEach(function (link, index) {//gets all links from page
+								linkList.push({ href: link.href, innerHTML: link.innerHTML });
+								console.log(index + "|" + aTags.length - 1 + "|" + c1);
+								if (index === aTags.length - 1 && c1 === 0) {
+									console.log(linkList);
+									sendResponse(linkList);
+									return true;
+								}
+							});
+						}
 					}
 				});//end of link getting code for frames
-				frame.document.body.innerHTML = frameBody.innerHTML.replace(new RegExp('(?<!<[^>]*)' + (request.checkboxStatus[1]? "\b"+request.searchTerm+"\b": request.searchTerm),(request.checkboxStatus[0]? "g":"gi")), replacement);//highlights keyword on frames
-				
+				frame.document.body.innerHTML = frameBody.innerHTML.replace(new RegExp('(?<!<[^>]*)' + (request.checkboxStatus[1] ? "\b" + request.searchTerm + "\b" : request.searchTerm), (request.checkboxStatus[0] ? "g" : "gi")), replacement);//highlights keyword on frames
+
 			};
-			
+
 		});
-		var aTags = document.getElementsByTagName('a');
-		
-		Array.from(aTags).forEach(function(link,index) {//gets all links from page
-			linkList.push({href: link.href, content: link.innerHTML});
-			console.log(index+"|"+aTags.length-1+"|"+c1);
-			if(index===aTags.length-1&&c1===0) {
+
+		Array.from(aTags).forEach(function (link, index) {//gets all links from page
+			linkList.push({ href: link.href, innerHTML: link.innerHTML });
+			console.log(index + "|" + aTags.length - 1 + "|" + c1);
+			if (index === aTags.length - 1 && c1 === 0) {
 				console.log(linkList);
 				linkList.push(window.location.href);
 				sendResponse(linkList);
 			}
 		});
 
-		
+
 		return true;
 
 	});
