cf53b95043313bf0cd56099acae76f06faaec95c
ericpedley@gmail.com <ericpedley@gmail.com>
2019-11-27 16:46:02 -0800

    implementing recursive strategy

--- content.js
+++ content.js
@@ -21,13 +21,13 @@ chrome.runtime.onMessage.addListener(
 					let frameLinks = Array.from(frameBody.getElementsByTagName("a"));//gets all links from frames on page
 					let c2 = 0;
 					frameLinks.forEach((link) => {
-						linkList.push({ href: link.href, innerHTML: link.innerHTML });
+						linkList.push({ href: processLink(link.href), innerHTML: link.innerHTML });
 						c2++;
 						if (c1 == frames.length && c2 == frameLinks.length) {
 							linkList.push(window.location.href);
 							if (aTags !== null) {
 								Array.from(aTags).forEach(function (link, index) {//gets all links from page
-									linkList.push({ href: link.href, innerHTML: link.innerHTML });
+									linkList.push({ href: processLink(link.href), innerHTML: link.innerHTML });
 									if (index === aTags.length - 1 && c1 === 0) {
 										console.log("LinkList from frames loop:" + linkList);
 										sendResponse(linkList);
@@ -47,11 +47,11 @@ chrome.runtime.onMessage.addListener(
 		console.log(aTags);
 		console.log("aTags Length:" + aTags.length);
 		Array.from(aTags).forEach(function (link, index) {//gets all links from page
-			linkList.push({ href: link.href, innerHTML: link.innerHTML });
+			linkList.push({ href: processLink(link.href), innerHTML: link.innerHTML });
 			console.log(new String("" + index + "|" + aTags.length - 1 + "|" + c1));
 			if (index === aTags.length - 1 && c1 === 0) {
 				console.log("linkListNotFromFramesLoop:" + linkList);
-				linkList.push(window.location.href);
+				linkList.push(processLink(window.location.href));
 				sendResponse(linkList);
 			}
 		});
@@ -61,3 +61,20 @@ chrome.runtime.onMessage.addListener(
 		return true;
 
 	});
+
+function processLink(string) {//removes everything following a "#" in a string, including the "#" itself
+	let i = string.indexOf("#");
+	if (!string.includes("http")) {
+		string = processLink(window.location.href) + string;
+		console.log("doing my job");
+		console.log(string);
+	}
+	if(string.charAt(string.length-1)==="/") {
+		string=string.substring(0,string.length-1);
+	}
+	if (i === -1) {
+		return string
+	} else {
+		return string.substring(0, i);
+	}
+}
--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     $('body').on('click', 'a', function () {//makes all a tags open new tab when clicked on
         chrome.tabs.create({ url: $(this).attr('href') });
         return false;
@@ -22,41 +21,88 @@ $(document).ready(function () {
                     function (response) {//response is an array containing the hrefs and innerhtmls of all the links on the page
                         let wordRegex = new RegExp('(?<!<[^>]*)' + (wordsChecked ? "\b" + search + "\b" : search), (caseChecked ? "g" : "gi"));
                         let winhref = response.pop();
-                        console.log("iterations: " + iterations);
-                        processLinks(response, winhref, wordRegex, iterations, []);
+                        response.map(function (element) { element["iteration"] = iterations });
+                        console.log(response);
+                        getRecursive(winhref, response, [winhref], wordRegex);
+                        //processLinks(response, winhref, wordRegex, iterations, []);
                     });
             });
         }
-
     });
 });
+
+
+getRecursive = (winhref, untestedlist, testedlist, wordRegex) => {
+    if (untestedlist.length > 0) {
+        console.log("length greater than zero");
+        let link = untestedlist.pop();
+        let href = link.href;
+        if (testedlist.indexOf(href) === -1) {
+            console.log("got here");
+            testedlist.push(href);
+            console.log(testedlist);
+            try {
+                $.get(href, null, function (text) {
+                    console.log("got there");
+                    //do stuff to add keyword to popup html
+                    if (wordRegex.test(text)) {//if the website html contains the keyword
+                        console.log("ok wtf");
+                        document.getElementById("linksHolder").innerHTML += "<br> <a href = '" + link.href + "'>" + link.innerHTML + "</a> <br>";//add to html of popup
+                    }
+                    if (link.iteration > 1) {
+                        let matches = text.matchAll(/<a.+?href="([^"]+)".*?>(.+?)<\/a>/sg);//regex tester: https://regex101.com/r/fMMH7H/1/
+                        for (const match of matches) {
+                            let newhref = processLink(match[1]);
+                            if(!newhref.includes("http")) {
+                                newhref=winhref+newhref;
+                            }
+                            if (testedlist.indexOf(newhref) === -1) 
+                                untestedlist.push({ href: newhref, innerHTML: match[2] });
+                        }
+                        getRecursive(href, untestedlist, testedlist, wordRegex);
+                    } else {
+                        getRecursive(winhref, untestedlist, testedlist, wordRegex);
+                    }
+                });
+            } catch (err) {
+                console.log("caught an error!");
+                getRecursive(href, untestedlist, testedlist, wordRegex);
+            }
+        } else {
+            getRecursive(winhref, untestedlist, testedlist, wordRegex);
+        }
+    }
+}
+
+
+
 processLinks = (linkList, winhref, wordRegex, numIterations, alreadyOpened) => {//linklist is what the jquery selector for tags returned
-    
+
     if (numIterations > 0) {
-        console.log(numIterations);
-        document.getElementById("linksHolder").innerHTML += "<br>Links from iteration "+numIterations+" at "+winhref+"</br>";
+        //console.log(numIterations);
+        document.getElementById("linksHolder").innerHTML += "<br>Links from iteration " + numIterations + " at " + winhref + "</br>";
         Array.from(linkList).forEach(function (link, index) {//for each link on the page
-            console.log(winhref);
-            let linkNoHash = rmvHash(link.href);//one of the links on the page
-            if (winhref !== linkNoHash&&!linkNoHash.includes("mailto")&&!alreadyOpened.includes(linkNoHash)) {//tests whether to consider opening this link on the open page
-                if(!link.href.includes("http")) {
-                    link.href=winhref+link.href;
+            //console.log(winhref);
+            let linkNoHash = processLink(link.href);//one of the links on the page
+            if (winhref !== linkNoHash && !linkNoHash.includes("mailto") && !alreadyOpened.includes(linkNoHash)) {//tests whether to consider opening this link on the open page
+                if (!link.href.includes("http")) {
+                    link.href = winhref + link.href;
                 }
                 $.get(link.href, null, function (text) {//get website html code as variable text
                     alreadyOpened.push(link.href);
-                    let noHash = rmvHash(link.href);//the link being opened from the webpage without the hash at the end and the stuff after
+                    let noHash = processLink(link.href);//the link being opened from the webpage without the hash at the end and the stuff after
                     //console.log(noHash+"is getting a get request from" + winhref + "on recursive iteration" + numIterations +", index "+index);
                     if (wordRegex.test(text)) {//if the website html contains the keyword
                         document.getElementById("linksHolder").innerHTML += "<br> <a id = 'link" + index + "' href = '" + link.href + "'>" + link.innerHTML + "</a> <br>";//add to html of popup
                     }
                     let linkList2 = [];
                     let matches = text.matchAll(/<a.+?href="([^"]+)".*?>(.+?)<\/a>/sg);//regex tester: https://regex101.com/r/fMMH7H/1/
-                    for(const match of matches) {
-                        linkList2.push({href: match[1], innerHTML: match[2]});
+                    for (const match of matches) {
+                        linkList2.push({ href: match[1], innerHTML: match[2] });
                     }
                     //console.log(linkList2);
                     console.log(alreadyOpened);
-                    processLinks(linkList2, linkNoHash, wordRegex, numIterations - 1,alreadyOpened);//take a recursive step using the list of all a tags from this page
+                    processLinks(linkList2, linkNoHash, wordRegex, numIterations - 1, alreadyOpened);//take a recursive step using the list of all a tags from this page
                 });
             }
         });
@@ -66,11 +112,19 @@ processLinks = (linkList, winhref, wordRegex, numIterations, alreadyOpened) => {
     }
 };
 
-function rmvHash(string) {//removes everything following a "#" in a string, including the "#" itself
-    let i = string.indexOf("#");
-    if(i===-1) {
-        return string
-    } else {
-        return string.substring(0,i);
-    }
+function processLink(string) {//removes everything following a "#" in a string, including the "#" itself
+	let i = string.indexOf("#");
+	if (!string.includes("http")) {
+		string = processLink(window.location.href) + string;
+		console.log("doing my job");
+		console.log(string);
+	}
+	if(string.charAt(string.length-1)==="/") {
+		string=string.substring(0,string.length-1);
+	}
+	if (i === -1) {
+		return string
+	} else {
+		return string.substring(0, i);
+	}
 }
