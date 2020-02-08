301aa63bd846921f8e4c073accfefaff82b586e5
ericpedley@gmail.com <ericpedley@gmail.com>
2020-02-07 22:34:28 -0800

    changed processlink helper method code, now it works on test case

--- popup.js
+++ popup.js
@@ -23,9 +23,9 @@ $(document).ready(function () {
                         let winhref = response.pop();
                         console.log(response);
                         response.map(function (element) { element["iteration"] = iterations });
-                        
+
                         getRecursive(winhref, response, [winhref], wordRegex);
-                        
+
                     });
             });
         }
@@ -35,7 +35,7 @@ $(document).ready(function () {
 /*
 Format for untestedlist:
 {
-    {href:"href string here", innerHTML:"innerhtml string here", iteration:"iteration number"},
+    {href:"href string here", innerHTML:"innerhtml string here", iteration:"iteration number", parent:"parent page url"},
     *more elements w/ same structure as above*
 }
 iteration number explanation:
@@ -48,18 +48,18 @@ function getRecursive(winhref, untestedlist, testedlist, wordRegex) {//untestedl
         let link = untestedlist.pop();// format: {href, innerhtml, iteration}
         let href = link.href;
         if (testedlist.indexOf(href) === -1) {//if this link hasn't already been searched
-            console.log(`the list doesn't already contain this link: ${href}, going to push it to list`);
+            console.log(`the list doesn't already contain this link: ${href} from ${link.parent}, going to push it to list`);
             testedlist.push(href);
             console.log(testedlist);
-            $.get(href,null, function (text) {
-                console.log(`successful GET request on ${href}`);
+            $.get(href, null, function (text) {
+                //console.log(`successful GET request on ${href}`);
                 //do stuff to add keyword to popup html
                 if (wordRegex.test(text)) {//if the website html contains the keyword
-                    console.log("found link that contains keyword, adding to popup");
+                    //console.log("found link that contains keyword, adding to popup");
                     document.getElementById("linksHolder").innerHTML += "<br> <a href = '" + link.href + "'>" + link.innerHTML + "</a> <br>";//add to html of popup
                 }
                 if (link.iteration > 1) {//if the iteration number of the link is over 1
-                    console.log("link iteration is greater than one");
+                    //console.log("link iteration is greater than one");
                     let matches = text.matchAll(/<a.+?href="([^"]+)".*?>(.+?)<\/a>/sg);/*regex for all a tags on page
                     output format: 
                         returns array of arrays. Format(where n is the index of any element in the array):
@@ -68,20 +68,23 @@ function getRecursive(winhref, untestedlist, testedlist, wordRegex) {//untestedl
                         matches[n][2]: innerhtml of a tag
                     regex tester: https://regex101.com/r/fMMH7H/1/    */
                     for (const match of matches) {
-                        console.log(`match: ${match}`);
-                        let newhref = processLink(match[1], href);
-                        if (!newhref.includes("http")) {//this means it's an internal link. This will definitely break if an href is in the form "../imaginary-page"
-                            console.log(href+"|"+newhref+"|"+winhref);
-                            newhref = href + newhref;
+                        //console.log(`match: ${match}`);
+                        let base = getBase(href);
+                        let unprocessed = match[1];
+                        let newhref = processLink(unprocessed, base);
+                        // if (!newhref.includes("http")) {//this means it's an internal link. This will definitely break if an href is in the form "../imaginary-page"
+                        //     newhref = base + newhref;
+                        //}
+                        if (testedlist.indexOf(newhref) === -1) {
+                            untestedlist.push({ href: newhref, innerHTML: match[2], parent: base });
+                            console.log(`pushing ${unprocessed} to list as ${newhref} from ${href} that has base ${base}, which came from regex match ${match}`);
                         }
-                        if (testedlist.indexOf(newhref) === -1)
-                            untestedlist.push({ href: newhref, innerHTML: match[2] });
                     }
                     getRecursive(href, untestedlist, testedlist, wordRegex);//call function with new elements added to list, and
                 } else {
                     getRecursive(winhref, untestedlist, testedlist, wordRegex);//call function again with same conditions, except untestedlist is one element shorter
                 }
-            }).fail(function(error) {//if the get request to the link fails, continue without searching for the keyword or more links
+            }).fail(function (error) {//if the get request to the link fails, continue without searching for the keyword or more links
                 console.log(error);
                 return getRecursive(winhref, untestedlist, testedlist, wordRegex);
             });
@@ -91,25 +94,29 @@ function getRecursive(winhref, untestedlist, testedlist, wordRegex) {//untestedl
         }
     } else {//when function is done
         document.getElementById("linksHolder").innerHTML += "<br>Done searching links<br>";
-        console.log("tested list: "+testedlist);
+        console.log("tested list: " + testedlist);
     }
 }
 
-function processLink(string, winhref) {//removes everything following a "#" in a string, including the "#" itself
-    console.log(string + "|" + winhref);
-    let i = string.indexOf("#");
-    if (!string.includes("http")&&!string.includes("#")) {
-        console.log("process link recursing");
-        string = processLink(winhref,"") + string;
-        console.log("doing my job");
-        console.log(string);
+/**
+ * url is something like /EricPedley?foo=bar 
+ * base is something like https://github.com/
+ * return will be something like https://github.com/EricPedley
+ */
+function processLink(url, base) {
+    let i = url.lastIndexOf("/");
+    if (i === url.length - 1) {// if there's stuff after the last slash
+        url = url.substring(0, i+1);//then delete that shit because without deleting it duplicate urls would be added to the tested list
     }
-    if (string.charAt(string.length - 1) === "/") {
-        string = string.substring(0, string.length - 1);
+    if (url.includes("http")) {//if the link isn't external
+        return url;//then it's complete, just return it
+    } else {//if it's an internal link
+        return base+url;//concatenate it with the base url and return that
     }
-    if (i === -1) {
-        return string;
-    } else {
-        return string.substring(0, i);
-    }
+}
+
+function getBase(url) {
+    let i = url.indexOf("//");
+    let j = url.indexOf("/", i + 2);
+    return url.substring(0, j + 1);
 }
